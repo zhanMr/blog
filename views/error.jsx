8f235d632ae49899ea38714223ceb473b12f8ec3
詹庆祥 <zqx1028_love@126.com>
2016-03-16 20:48:50 +0800

    add file

---
+++ views/error.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var HelloMessage = React.createClass({
+    render: function() {
+        return <div>123</div>
+    }
+});
+
+module.exports = HelloMessage;
---
+++ views/index.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var Index = React.createClass({
+        render: function() {
+            return (
+                   <div>hello node</div>
+                )
+        }
+});
+module.exports = Index;
