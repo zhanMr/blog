68f3344940be5f41d0aee75a64754fda39acb559
詹庆祥 <zqx1028_love@126.com>
2016-04-03 17:09:48 +0800

    update page number

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const db = require('../service/db');
 // /(index|^\/$)/ 匹配/和/index
 router.get('/', (req, res) => {
     let page = parseInt(req.query.page) || 1;
-    let num = 3;
+    let num = 9;
     let sql = 'select * from content order by id desc limit ' + (page - 1)*num + ',' + ((page - 1)*num + 9);
     db(sql, (err, rows, fields) => {
         res.render('index', { title: '首页' , data: rows, page: page, total: 90});
